16fb122af0d904bbe0dc45326a375df55e1d86cc
alej4ndro-cm <90579820+alej4ndro-cm@users.noreply.github.com>
2025-08-27 02:12:17 +0000

    style: update config file formatting

--- src/maintenance.js
+++ src/maintenance.js
@@ -1,2 +1,3 @@
 // refactor: separate CPU/GPU paths @ 2025-08-24 02:28:23Z
 // refactor: move constants to config @ 2025-08-27 02:12:09Z
+// style: update config file formatting @ 2025-08-27 02:12:17Z
