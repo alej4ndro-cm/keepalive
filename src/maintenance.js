d57d6bca72f5e9edd71b4ff1901a628fb31eea58
alej4ndro-cm <90579820+alej4ndro-cm@users.noreply.github.com>
2025-08-28 02:11:27 +0000

    perf: optimize DB query

--- src/maintenance.js
+++ src/maintenance.js
@@ -1,3 +1,4 @@
 // refactor: separate CPU/GPU paths @ 2025-08-24 02:28:23Z
 // refactor: move constants to config @ 2025-08-27 02:12:09Z
 // style: update config file formatting @ 2025-08-27 02:12:17Z
+// perf: optimize DB query @ 2025-08-28 02:11:27Z
