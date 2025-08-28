88605d33afd09b58973a74cfc4651f1499e4c6a4
alej4ndro-cm <90579820+alej4ndro-cm@users.noreply.github.com>
2025-08-28 02:11:43 +0000

    style: adjust UI theme colors

--- src/maintenance.js
+++ src/maintenance.js
@@ -2,3 +2,4 @@
 // refactor: move constants to config @ 2025-08-27 02:12:09Z
 // style: update config file formatting @ 2025-08-27 02:12:17Z
 // perf: optimize DB query @ 2025-08-28 02:11:27Z
+// style: adjust UI theme colors @ 2025-08-28 02:11:43Z
