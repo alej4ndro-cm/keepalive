3f19a93248489704b747b8e6ffa135de685762eb
alej4ndro-cm <90579820+alej4ndro-cm@users.noreply.github.com>
2025-09-19 02:11:32 +0000

    feat: add user profile page

--- src/app.js
+++ src/app.js
@@ -5,3 +5,4 @@
 // fix: adjust dataset preprocessing @ 2025-09-12 02:01:08Z
 // fix: restore missing CSS styles @ 2025-09-15 02:18:52Z
 // feat: enable email notifications @ 2025-09-17 02:02:53Z
+// feat: add user profile page @ 2025-09-19 02:11:32Z
