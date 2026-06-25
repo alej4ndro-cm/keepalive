901887834feaaca65c18b9dbf27960203e2a5ff1
alej4ndro-cm <90579820+alej4ndro-cm@users.noreply.github.com>
2026-06-25 04:43:27 +0000

    refactor: separate DB migrations

--- src/maintenance.js
+++ src/maintenance.js
@@ -115,3 +115,4 @@
 // perf: reduce image sizes @ 2026-06-22 05:38:36Z
 // refactor: extract inference service @ 2026-06-23 04:38:59Z
 // refactor: move constants to config @ 2026-06-25 04:43:18Z
+// refactor: separate DB migrations @ 2026-06-25 04:43:27Z
